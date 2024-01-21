c935a1ef358b7fa134402e3ec59c2956f6cc0ad1
roku36 <roku36000@gmail.com>
2024-01-21 21:31:56 +0900

    limit dice range

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ fn main() {
         .insert_resource(Gravity(Vec3::NEG_Y * 80.0))
         .add_systems(Startup, setup)
         .add_systems(Update, movement)
+        .add_systems(Update, apply_limit)
         .run();
 }
 
@@ -21,37 +22,60 @@ fn main() {
 #[derive(Component)]
 struct MovementAcceleration(Scalar);
 
+#[derive(Component)]
+struct LimitRadius(Scalar);
+
 fn setup(
     mut commands: Commands,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
     let cube_mesh = meshes.add(Mesh::from(shape::Cube { size: 1.0 }));
+    let cylinder_mesh = meshes.add(Mesh::from(shape::Cylinder { radius: 10.0, height: 1.0, resolution: 256, segments: 1 }));
 
-    // Ground
+    // cylinder for dice dish
     commands.spawn((
         PbrBundle {
-            mesh: cube_mesh.clone(),
-            material: materials.add(Color::rgb(0.7, 0.7, 0.8).into()),
-            transform: Transform::from_xyz(0.0, -2.0, 0.0).with_scale(Vec3::new(100.0, 1.0, 100.0)),
+            mesh: cylinder_mesh.clone(),
+            material: materials.add(Color::rgb(0.0, 0.2, 0.0).into()),
+            transform: Transform::from_xyz(0.0, 0.0, 0.0),
             ..default()
         },
         RigidBody::Static,
-        Collider::cuboid(1.0, 1.0, 1.0),
+        Collider::cylinder(1.0, 10.0),
     ));
+    // commands.spawn((
+    //     PbrBundle {
+    //         mesh: circle_mesh.clone(),
+    //         material: materials.add(Color::rgb(0.2, 0.7, 0.9).into()),
+    //         transform: Transform::from_xyz(0.0, 1.0, 0.0),
+    //         ..default()
+    //     },
+    //     RigidBody::Dynamic,
+    //     Collider::cylinder(0.5, 1.0),
+    // ));
+
 
     let cube_size = 2.0;
-    let cube_gap = 2.0;
+    let cube_gap = 0.5;
 
     let mut rng = rand::thread_rng();
 
     // spawn five cubes
-    let cube_pos: [Vec3; 5] = [
-        Vec3::new(0.0, 5.0, 0.0),
-        Vec3::new(1.0, 5.0, 1.0),
-        Vec3::new(1.0, 5.0, -1.0),
-        Vec3::new(-1.0, 5.0, 1.0),
-        Vec3::new(-1.0, 5.0, -1.0),
+    let cube_height = 15.0;
+    // let cube_pos: [Vec3; 5] = [
+    //     Vec3::new(0.0, 5.0, 0.0),
+    //     Vec3::new(1.0, 5.0, 1.0),
+    //     Vec3::new(1.0, 5.0, -1.0),
+    //     Vec3::new(-1.0, 5.0, 1.0),
+    //     Vec3::new(-1.0, 5.0, -1.0),
+    // ];
+    let cube_pos = [
+        Vec3::new(0.0, cube_height, 0.0),
+        Vec3::new(1.0, cube_height, 1.0),
+        Vec3::new(1.0, cube_height, -1.0),
+        Vec3::new(-1.0, cube_height, 1.0),
+        Vec3::new(-1.0, cube_height, -1.0),
     ];
     for pos in cube_pos {
         let translation= pos * (cube_size + cube_gap);
@@ -69,6 +93,7 @@ fn setup(
             RigidBody::Dynamic,
             Collider::cuboid(1.0, 1.0, 1.0),
             MovementAcceleration(10.0),
+            LimitRadius(7.0),
         ));
     }
 
@@ -115,6 +140,20 @@ fn movement(
         if direction != Vector::ZERO {
             linear_velocity.x += direction.x * movement_acceleration.0 * delta_time;
             linear_velocity.z += direction.z * movement_acceleration.0 * delta_time;
+        }
+    }
+}
+
+fn apply_limit(mut query: Query<(&LimitRadius, &Position, &mut LinearVelocity)>) {
+    for (limit_radius, position, mut linear_velocity) in &mut query {
+        // if euclidean_distance(position.xy, Vec3::ZERO) > limit_radius.0 {
+        if position.xz().length() > limit_radius.0 {
+            let normalized = position.xz().normalize();
+            let dot_velocity = normalized.dot(linear_velocity.xz());
+            if dot_velocity > 0.0 {
+                linear_velocity.x -= dot_velocity * normalized.x;
+                linear_velocity.z -= dot_velocity * normalized.y;
+            }
         }
     }
 }
